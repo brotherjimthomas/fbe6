ea8105618471dacb58f849e78ba8ef95fd0508b2
Jim Thomas <jim.thomas@nwea.org>
2017-07-25 09:27:11 -0700

    Stuck in ch7

--- crimemap.py
+++ crimemap.py
@@ -1,5 +1,6 @@
 # from dbhelper import DBHelper
 from flask import Flask
+import json
 from flask import render_template
 from flask import request
 
@@ -15,12 +16,9 @@ DB = DBHelper()
 
 @app.route("/")
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print(e)
-        data = None
-    return render_template("home.html", data=data)
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes)
 
 
 @app.route("/add", methods=["POST"])
@@ -39,6 +37,17 @@ def clear():
         DB.clear_all()
     except Exception as e:
         print(e)
+    return home()
+
+
+@app.route("/submitcrime", methods=["POST"])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 
